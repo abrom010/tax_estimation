b1961e054dfeec1c17b4c89c5e6c13a116c1640c
Aaron <abrom010@fiu.edu>
2022-04-29 19:47:05 -0400

    update tax calculation more accurate

--- calculating.py
+++ calculating.py
@@ -19,17 +19,34 @@ def calculate_tax(bundle):
 		self_employed_tax = payment * self_employed_percentage
 		#print("Self employed tax: " + str(self_employed_tax))
 		deduction = self_employed_tax / 2
-		net_income += payment - deduction
 		income_tax = 0
-		for bracket in tax_brackets:
-			if net_income < float(bracket):
-				# temporarily simple
-				income_tax = float((payment - deduction)) * float(tax_brackets[bracket])
+
+		# net_addition = payment - deduction
+		# for bracket in tax_brackets:
+		# 	if net_income + net_addition < float(bracket):
+		# 		income_tax = float((payment - deduction)) * float(tax_brackets[bracket])
+		# 		net_income += net_addition
+		# 		break
+		
+		net_addition = payment - deduction
+		for i, bracket in enumerate(tax_brackets):
+			if net_income < float(bracket) and net_income + net_addition >= float(bracket) and bracket != "over":
+				money_for_first_bracket = float(bracket) - net_income
+				income_tax = float(money_for_first_bracket * float(tax_brackets[bracket]))
+				income_tax += float((net_addition - money_for_first_bracket) * float(list(tax_brackets.values())[i + 1]))
+				net_income += net_addition
 				break
-		#print("Income tax: " + str(income_tax))
+			elif net_income + net_addition < float(bracket):
+				income_tax = float((payment - deduction)) * float(tax_brackets[bracket])
+				net_income += net_addition
+				break			
+		
 		tax_for_payment = self_employed_tax + income_tax
 		total_tax += tax_for_payment
 		print("Payment: $" + str(payment) + "\t\tTax: $" + str(round(tax_for_payment, 2)))
 
+	print()
 	print("Gross income: $" + str(gross_income))
+	print("Net income: $" + str(round(net_income, 2)))
+	print()
 	print("Total tax owed: $" + str(round(total_tax, 2)))
